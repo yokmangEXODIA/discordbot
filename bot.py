d31fd6e6d97443da4c389c71f85b4e990a7c2fdb
KawaiTrol1 <jeongathanasio@naver.com>
2021-01-20 15:06:46 +0900

    1
    1

--- bot.py
+++ bot.py
@@ -3,14 +3,15 @@ from discord.ext import commands
 import os
 
 client = commands.Bot(command_prefix = '-')
+prefix = "~!"
 
 @client.event
 async def on_ready():
 
   # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
-  await client.change_presence(status=discord.Status.online)
+  await client.change_presence(status=discord.Status.dnd)
 
-  await client.change_presence(activity=discord.Game(name="게임 하는중"))
+  await client.change_presence(activity=discord.Game(name="~!오클아 도움말"))
   #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
@@ -18,4 +19,4 @@ async def on_ready():
   print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
 
 
-client.run(os.environ['token'])
+client.run(os.environ['ODAxMzE2MDczNTA4NDM4MDE2.YAe5vw.dipcC5eWbeG0DrMy8PzKogS9KQ8'])
